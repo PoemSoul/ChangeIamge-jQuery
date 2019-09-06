0314482ea25e824b9384dc9cc38b587cc512398b
诗魂 <poetrysoulfly@gmail.com>
2019-09-06 09:49:51 +0800

    Update CI.js

--- js/CI.js
+++ js/CI.js
@@ -10,7 +10,7 @@ $(function(){
     //实现轮播
 
     function action(){
-        $(".img").fadeOut(1000);
+        $(".img").eq(img_index).siblings().fadeOut(1000);
         $(".img").eq(img_index).fadeIn(2000);
         $(".controls ul li").eq(img_index).css("opacity","1");
         $(".controls ul li").eq(img_index).siblings().css("opacity","0.4");
@@ -24,19 +24,19 @@ $(function(){
 
     $(".imglist").mouseover(function(){
         window.clearInterval(interval);
-    })
+    });
 
     $(".imglist").mouseout(function(){
         interval=setInterval(changeImg,5000);
-    })
+    });
 
 
     $(".controls ul li").click(function(){
         window.clearInterval(interval);
-        img_index=$(this).index();
+        img_index=$(this).index();  //$(this) 当前的被单击的这个li；index() 返回一组元素中，某个元素的index，从0开始
         action();
         interval=setInterval(changeImg,5000);
-    })
+    });
 
     $(".left").click(function(){
         window.clearInterval(interval);
@@ -46,13 +46,12 @@ $(function(){
         }
         action();
         interval=setInterval(changeImg,5000);
-    })
+    });
 
     $(".right").click(function(){
         window.clearInterval(interval);
-        img_index=++img_index % 4;
-        action();
+        changeImg();
         interval=setInterval(changeImg,5000);
-    })
+    });
 })
     
